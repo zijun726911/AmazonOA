880325f36c36db3955721c5b51527cd02ec56afe
zijun726911 <zijun112233@163.com>
2021-09-12 13:06:49 -0700

    lc937

--- src/main/java/oa/lc937/Solution.java
+++ src/main/java/oa/lc937/Solution.java
@@ -8,46 +8,38 @@ public class Solution {
     //https://leetcode.com/problems/reorder-data-in-log-files/
 
     public String[] reorderLogFiles(String[] logs) {
-        PriorityQueue<String> letterQueue = new PriorityQueue((o1,  o2)->{
-            String str1=(String) o1;
-            String str2=(String) o2;
-
-            String[] s1 = str1.split(" ", 2);
-            String[] s2 = str2.split(" ", 2);
-
-            String id1=s1[0];
-            String content1=s1[1];
-            String id2=s2[0];
-            String content2=s2[1];
-
-            int contentCmpRes=content1.compareTo(content2);
-            if(contentCmpRes!=0) {
-                return contentCmpRes;
-            }else {
-                return id1.compareTo(id2);
-            }
-
-        });
-
 
         List<String> digitLogs=new ArrayList<>();
+        List<String> letterLogs=new ArrayList<>();
+
         for (String log : logs) {
             char lastChar = log.charAt(log.length() - 1);
             if(Character.isDigit(lastChar)){ //Digit-logs
                 digitLogs.add(log);
             }else{//Letter-logs
-                letterQueue.offer(log);
+                letterLogs.add(log);
             }
         }
 
-        List<String> res=new ArrayList<>();
-        while (!letterQueue.isEmpty()){
-            res.add(letterQueue.poll());
-        }
+        letterLogs.sort((str1,  str2)->{
 
-        res.addAll(digitLogs);
+            int firstSpace1 = str1.indexOf(' ');
+            int firstSpace2 = str2.indexOf(' ');
 
-        return res.toArray(new String[0]);
+            int contentCmpRes=str1.substring(firstSpace1+1).compareTo(str2.substring(firstSpace2+1));
+            if(contentCmpRes!=0) {
+                return contentCmpRes;
+            }else {
+                return str1.substring(0,firstSpace1).compareTo(str2.substring(0,firstSpace2));
+            }
+
+        });
+
+
+
+        letterLogs.addAll(digitLogs);
+
+        return letterLogs.toArray(new String[0]);
 
 
 
